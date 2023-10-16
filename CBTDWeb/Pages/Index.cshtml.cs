b22200b412f8762aeb8e459e038405f2f894455d
buxxmoney <87129838+buxxmoney@users.noreply.github.com>
2023-10-15 20:22:33 -0600

    added filtering

--- CBTDWeb/Pages/Index.cshtml.cs
+++ CBTDWeb/Pages/Index.cshtml.cs
@@ -9,15 +9,18 @@ namespace CBTDWeb.Pages
     {
         private readonly UnitOfWork _unitOfWork;
         public IEnumerable<Product> objProductList;
+        public IEnumerable<Category> objCategoryList;
         public IndexModel(UnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
             objProductList = new List<Product>();   
+            objCategoryList = new List<Category>();
         }
 
         public IActionResult OnGet()
         {
             objProductList = _unitOfWork.Product.GetAll(null, includes: "Category,Manufacturer");
+            objCategoryList = _unitOfWork.Category.GetAll(null, c => c.DisplayOrder, null);
             return Page();
         }
     }
